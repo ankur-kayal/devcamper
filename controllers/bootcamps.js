c4bf2ef942e6755f81e8154f3a8c693451b75852
Ankur Kayal <ankurkayal1234@gmail.com>
2021-04-11 19:33:32 +0530

    Advanced filtering features

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,44 @@ const ErrorResponse = require('../utils/ErrorResponse');
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = ['select', 'sort'];
+
+  // Loop over removeFields and exclude them from query
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  console.log(reqQuery);
+
+  // Create query string and create operators like ($gt, $gte etc)
+  let queryStr = JSON.stringify(reqQuery).replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  // Finding resource
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Executing query
+  const bootcamps = await query;
 
   res
     .status(200)
