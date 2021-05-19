32b62692d16a2ca6a14694de10f2325c6c5d8dc6
Ankur Kayal <ankurkayal1234@gmail.com>
2021-05-19 23:55:47 +0530

    Reviews funcitonality completed

--- seeder.js
+++ seeder.js
@@ -11,6 +11,7 @@ dotenv.config({ path: './config/config.env' });
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -32,12 +33,17 @@ const users = JSON.parse(
   fs.readFileSync(path.join(__dirname, '_data', 'users.json'), 'utf-8')
 );
 
+const reviews = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '_data', 'reviews.json'), 'utf-8')
+);
+
 // Import to DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
     console.log('Data Imported...'.green);
     process.exit();
   } catch (err) {
@@ -51,6 +57,7 @@ const deleteData = async () => {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data deleted...'.red);
     process.exit();
   } catch (err) {
